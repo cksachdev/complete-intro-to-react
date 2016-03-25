24085b9b5709b0d54ccc565988c16ad8de20517e
Chetan Sachdev <cksachdev@gmail.com>
2016-03-25 14:57:30 +0530

    Fixed step-9.. need to see the difference

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,16 +1,14 @@
 const React = require('react')
 const ReactDOM = require('react-dom')
 const Landing = require('./Landing')
-const ReactRouter = require('react-router')
-const { Router, Route, hashHistory } = ReactRouter
+const Search = require('./Search')
+const { Router, Route, hashHistory } = require('react-router')
 
-// const Router = ReactRouter.Router
-// const Route = ReactRouter.Route
-// const hashHistory = ReactRouter.hashHistory
-
-const App = () => {
+const App = () => (
   <Router history={hashHistory}>
   <Route path='/' component={Landing} />
+  <Route path='/search' component={Search} />
   </Router>
-}
+  )
+
 ReactDOM.render(<App />, document.getElementById('app'))
--- js/Landing.jsx
+++ js/Landing.jsx
@@ -1,13 +1,13 @@
-const Landing = function (){
-  return (
-    <div className='app-container'>
-    <div className='home-info'>
-    <h1 className='title'>svideo</h1>
-    <input className='search' type='text' placeholder='Search' />
-    <button className='browse-all'> or Browse All</button>
-    </div>
-    </div>
-    )
-  }
+const React = require('react')
+
+const Landing = () => (
+  <div className='app-container'>
+  <div className='home-info'>
+  <h1 className='title'>svideo</h1>
+  <input className='search' type='text' placeholder='Search' />
+  <button className='browse-all'> or Browse All</button>
+  </div>
+  </div>
+  )
 
   module.exports = Landing
