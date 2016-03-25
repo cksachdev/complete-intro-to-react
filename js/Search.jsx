6f7db32e2cbe45881a93ce5222b9ad709af41557
Chetan Sachdev <cksachdev@gmail.com>
2016-03-25 20:58:46 +0530

    Got code coverage working test cases using enzyme from airbnb show the lines which are not run using istanbul/nyc

--- js/Search.jsx
+++ js/Search.jsx
@@ -8,26 +8,29 @@ const Search = React.createClass({
       searchTerm: ''
     }
   },
-  handleSearchTermEvent (event){
-    this.setState({ searchTerm: event.target.value})
+  randomFunction (){
+    console.log('Some random function')
   },
-  render (){
+  handleSearchTermEvent (event) {
+    this.setState({ searchTerm: event.target.value })
+  },
+  render () {
     return (
       <div className='container'>
-      <header className='header'>
-      <h1 className='brand'>{this.state.searchTerm}</h1>
-      <input value={this.state.searchTerm} className='search-input' type='text'
-      placeholder='Search' onChange={this.handleSearchTermEvent}/>
-      </header>
-      <div className='shows'>
-      {data.shows
-        .filter((show) => `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0)
-        .map((show) => (
-          <ShowCard {...show} key={show.imdbID}/>
+        <header className='header'>
+          <h1 className='brand'>svideo</h1>
+          <input value={this.state.searchTerm} className='search-input' type='text' placeholder='Search' onChange={this.handleSearchTermEvent} />
+        </header>
+        <div className='shows'>
+          {data.shows
+            .filter((show) => `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0)
+            .map((show) => (
+              <ShowCard {...show} key={show.imdbID} />
           ))}
         </div>
-        </div>
-        )}
-  })
+      </div>
+    )
+  }
+})
 
 module.exports = Search
